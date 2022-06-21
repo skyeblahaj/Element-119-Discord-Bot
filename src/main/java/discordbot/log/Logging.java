eb585bb5b42257b17c835a54e0efeee38afae8ce
skyeblahaj <cyclonesgamer86@gmail.com>
2022-06-20 23:41:13 -0400

    prefix helper
    if user only types the prefix it will display a "getting started" menu. doing this in the logging class since the command registry can't register a command that is only the prefix

--- src/main/java/discordbot/log/Logging.java
+++ src/main/java/discordbot/log/Logging.java
@@ -1,5 +1,10 @@
 package discordbot.log;
 
+import java.awt.Color;
+
+import discordbot.utils.Functions;
+import discordbot.utils.Info;
+import net.dv8tion.jda.api.entities.MessageEmbed.Field;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -10,6 +15,11 @@ public class Logging extends ListenerAdapter {
 		System.out.println(event.getAuthor().getId() + "\n---\n" +
 						event.getChannel().toString() + "\n---\n" +
 						event.getMessage().getContentRaw() + '\n');
+		
+		//command exceptions
+		if (event.getMessage().getContentRaw().equals(Info.PREFIX)) {
+			Functions.Messages.sendEmbeded(event.getChannel(), Functions.Messages.buildEmbed("Commands", new Color(0x0000ff), new Field("To get started:", "Use '" + Info.PREFIX + "commands'", false)));
+		};
 	}
 	
 }
